3f0518ecdb0a5000cc70b5efe59fcfbeeb84c8d5
kaetahr <lostech@tuta.io>
2021-10-27 21:19:47 -0500

    Added dummy login page and portal home page

---
+++ portal-home.php
@@ -0,0 +1,28 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/general.css">
+    <link rel="stylesheet" href="styles/menu.css">
+    <script src="javascript/menu.js"></script>
+    <title>Menu de la Semana</title>
+</head>
+<body>
+    <header class="w3-display-container w3-black w3-teal w3-responsive" style="height: 120px;">
+        <h1 class="w3-display-middle">Cocina de Allison Thompson</h1>
+        <img class="w3-display-right" src="img/logo.png" alt="logo" width="60" height="60">
+    </header>
+    <nav class="w3-bar w3-xlarge w3-black w3-opacity w3-hover-opacity-off w3-top" id="myNavbar">
+        <a href="lista_pedidos.html" class="w3-bar-item w3-button w3-mobile">LISTA DE PEDIDOS</a>
+    </nav>
+    <main class="w3-container w3-sand">
+
+    </main>
+    <footer class="w3-container w3-teal w3-black w3-center">
+        <a href="https://www.facebook.com/La-Cocina-de-Allison-Thompson-102969278285943" class="fa fa-facebook w3-xxxlarge w3-padding-16"></a>
+    </footer>
+</body>
+</html>
